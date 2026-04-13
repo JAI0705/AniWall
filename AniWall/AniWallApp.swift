04641b073f4d12df34ddfe02f1997d61b0f11b3d
Jai0705 <Jai0705tiwari@gmail.com>
2026-04-13 19:20:23 +0530

    Initial Commit

---
+++ AniWall/AniWallApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniWallApp.swift
+//  AniWall
+//
+//  Created by Jai Tiwar on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AniWallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
